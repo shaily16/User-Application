24d6c7a89a2bc9f2016c27a4829190d99b15642b
saurbhmankar-sdn <saurbh.mankar@smartdatainc.net>
2020-11-11 22:47:45 +0530

    latest changes

--- User-Application/backend/api/helpers/response.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (code, status, message, data, token) {
-    var response = {};
-    response.code = code;
-    response.status = status;
-    response.message = message;
-    response.data = data;
-    return response;
-}
